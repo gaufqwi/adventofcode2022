1938f7a5afd7ac30b5067ced450a601b9ce0277b
jay <johnabloodworth3@gmail.com>
2022-12-02 05:14:28 -0500

    Day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -5,10 +5,83 @@ pub struct Problem();
 
 impl AOCProblem for Problem {
     fn part1(&self, input: AOCInput) -> String {
-        String::from("Part 1")
+        let mut score = 0;
+        for l in input.as_lines() {
+            let (opp, me) = l.split_once(" ").unwrap();
+            match me {
+                // rock
+                "X" => {
+                    score += 1;
+                    if opp == "A" {
+                        score += 3  // draw
+                    } else if opp == "C" {
+                        score += 6  // win
+                    }
+                }
+                // paper
+                "Y" => {
+                    score += 2;
+                    if opp == "B" {
+                        score += 3  // draw
+                    } else if opp == "A" {
+                        score += 6  // win
+                    }
+                }
+                // scissors
+                "Z" => {
+                    score += 3;
+                    if opp == "C" {
+                        score += 3  // draw
+                    } else if opp == "B" {
+                        score += 6  // win
+                    }
+                }
+                _ => panic!("Bad input / parsing")
+            }
+        }
+        score.to_string()
     }
 
     fn part2(&self, input: AOCInput) -> String {
-        String::from("Part 2")
+        let mut score = 0;
+        for l in input.as_lines() {
+            let (opp, me) = l.split_once(" ").unwrap();
+            match me {
+                // lose
+                "X" => {
+                    if opp == "A" {
+                        score += 3  // scissors < rock
+                    } else if opp == "B" {
+                        score += 1  // rock < paper
+                    } else {
+                        score += 2  // paper < scissors
+                    }
+                }
+                // draw
+                "Y" => {
+                    score += 3;
+                    if opp == "A" {
+                        score += 1  // rock = rock
+                    } else if opp == "B" {
+                        score += 2  // paper = paper
+                    } else {
+                        score += 3  // scissors = scissors
+                    }
+                }
+                // win
+                "Z" => {
+                    score += 6;
+                    if opp == "A" {
+                        score += 2  // paper > rock
+                    } else if opp == "B" {
+                        score += 3  // scissors > paper
+                    } else {
+                        score += 1  // rock > scissors
+                    }
+                }
+                _ => panic!("Bad input / parsing")
+            }
+        }
+        score.to_string()
     }
 } 
