d6f6237937be19b13a3e11eeb7fa925994b7ea49
jay <johnabloodworth3@gmail.com>
2022-12-04 01:05:21 -0500

    Day 4

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -5,10 +5,36 @@ pub struct Problem();
 
 impl AOCProblem for Problem {
     fn part1(&self, input: AOCInput) -> String {
-        String::from("Part 1")
+        let mut subsets = 0;
+        for l in input.as_lines() {
+            let (elfa, elfb) = l.split_once(",").unwrap();
+            let (elfa_s, elfa_e) = elfa.split_once("-").unwrap();
+            let (elfb_s, elfb_e) = elfb.split_once("-").unwrap();
+            let elfa_si = elfa_s.parse::<i32>().unwrap();
+            let elfa_ei = elfa_e.parse::<i32>().unwrap();
+            let elfb_si = elfb_s.parse::<i32>().unwrap();
+            let elfb_ei = elfb_e.parse::<i32>().unwrap();
+            if (elfa_si >= elfb_si && elfa_ei <= elfb_ei) || (elfb_si >= elfa_si && elfb_ei <= elfa_ei) {
+                subsets += 1;
+            }
+        }
+        subsets.to_string()
     }
 
     fn part2(&self, input: AOCInput) -> String {
-        String::from("Part 2")
+        let mut subsets = 0;
+        for l in input.as_lines() {
+            let (elfa, elfb) = l.split_once(",").unwrap();
+            let (elfa_s, elfa_e) = elfa.split_once("-").unwrap();
+            let (elfb_s, elfb_e) = elfb.split_once("-").unwrap();
+            let elfa_si = elfa_s.parse::<i32>().unwrap();
+            let elfa_ei = elfa_e.parse::<i32>().unwrap();
+            let elfb_si = elfb_s.parse::<i32>().unwrap();
+            let elfb_ei = elfb_e.parse::<i32>().unwrap();
+            if (elfa_si >= elfb_si && elfa_si <= elfb_ei) || (elfa_ei >= elfb_si && elfa_si <= elfb_ei) {
+                subsets += 1;
+            }
+        }
+        subsets.to_string()
     }
 } 
