95f241b8c39927982461d86b361b1f3b806cf117
jay <johnabloodworth3@gmail.com>
2022-12-06 04:35:13 -0500

    Day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -5,10 +5,44 @@ pub struct Problem();
 
 impl AOCProblem for Problem {
     fn part1(&self, input: AOCInput) -> String {
-        String::from("Part 1")
+        let mut buf = vec!['X', 'X', 'X', 'X'];
+        for (i, c) in input.raw().trim().chars().enumerate() {
+            rotate(&mut buf, 4, c);
+            //dbg!("{}", &buf);
+            if all_different(&buf, 4) && i >= 3 {
+                return (i + 1).to_string();
+            }
+        }
+        String::from("failure")
     }
 
     fn part2(&self, input: AOCInput) -> String {
-        String::from("Part 2")
+        let mut buf = vec!['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+        for (i, c) in input.raw().trim().chars().enumerate() {
+            rotate(&mut buf, 14, c);
+            //dbg!("{}", &buf);
+            if all_different(&buf, 14) && i >= 13 {
+                return (i + 1).to_string();
+            }
+        }
+        String::from("failure")
     }
-} 
+}
+
+fn rotate (buf : &mut Vec<char>, size : usize, c : char) {
+    for i in 0..(size-1) {
+        buf[i] = buf[i+1]
+    }
+    buf[size-1] = c;
+}
+
+fn all_different (buf : &Vec<char>, size : usize) -> bool {
+    for i in 0..(size-1) {
+        for j in (i+1)..size {
+            if buf[i] == buf[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
